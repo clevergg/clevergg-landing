d6da81ba7aef0b352a425d1b9b24685c92a4e238
clevergg <ast.gor11@gmail.com>
2026-08-26 06:32:07 +0300

    Initial commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en">
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
